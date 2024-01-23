390a758cdc00d624affb0848322ba3e8bc51e4c6
lorena <lorbalagera26@gmail.com>
2024-01-23 18:26:25 -0500

    archivos

--- ejerBootstrap/componentes Boostrap/ejer8C-B.js
+++
@@ -1,6 +0,0 @@
-/* Badges (Insignias): Mejora la galería de imágenes del ejercicio 6 
-agregando insignias que indiquen la cantidad de imágenes en la galería.
- Utiliza estilos de Bootstrap para las insignias y asegúrate de que se
-  vean bien en diferentes dispositivos.*/
-
-  
